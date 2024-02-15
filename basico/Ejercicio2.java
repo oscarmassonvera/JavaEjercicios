fdfaaa392267c3464d0a22729ca3946506436fc7
oscarmasson <oscarmasson@yahoo.com>
2024-02-15 11:41:15 -0500

    Ejercicio2

--- basico/Ejercicio2.java
+++ basico/Ejercicio2.java
@@ -5,7 +5,19 @@ public class Ejercicio2 {
     junto a su correspondiente traduccion al castellano. Las palabras
     deben estar distribuidas en dos columnas y alineadas a la izq.
     * */
+
     public static void main(String[] args) {
-        
+        System.out.println(
+            "Age \t Edad/Era"+ "\n" +
+            "Air \t Aire"+ "\n" +
+            "Answer \t Respuesta"+ "\n" +
+            "Apple \t Manzana"+ "\n" +
+            "Ball \t Pelota/Balon"+ "\n" +
+            "Bank \t Banco"+ "\n" +
+            "Bed \t Cama"+ "\n" +
+            "Bird \t Pajaro"+ "\n" +
+            "Chair \t Silla"+ "\n" +
+            "Corn \t Maiz"+ "\n"
+        );
     }
 }
