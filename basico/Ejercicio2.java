3b798f2b7446f07d850db7ef973e5dcd712711e8
oscarmasson <oscarmasson@yahoo.com>
2024-02-15 11:31:57 -0500

    Ejercicio2

---
+++ basico/Ejercicio2.java
@@ -0,0 +1,11 @@
+package org.ejercicios.basico;
+
+public class Ejercicio2 {
+    /* Escribe un programa que mueste por pantalla 10 palabras en ingles
+    junto a su correspondiente traduccion al castellano. Las palabras
+    deben estar distribuidas en dos columnas y alineadas a la izq.
+    * */
+    public static void main(String[] args) {
+        
+    }
+}
